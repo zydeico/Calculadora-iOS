b84dc7d8941b6e3f373b62eb6c5dd8b520c96400
Daniel Vázquez <daniel.prolite@gmail.com>
2019-06-27 16:26:13 -0500

    Files updated

---
+++ Controller/Home/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  iOS Calculator
+//
+//  Created by Daniel Vázquez on 6/27/19.
+//  Copyright © 2019 Bit Freelancer. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MAR: - Initialization
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MAR: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
