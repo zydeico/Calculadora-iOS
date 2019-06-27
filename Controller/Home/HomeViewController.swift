b416322508e5e03b2235e5c8f4e8cd57a72b115f
Daniel Vázquez <daniel.prolite@gmail.com>
2019-06-27 18:25:14 -0500

    Resolviendo conflictos de versionamiento xCode

--- Controller/Home/HomeViewController.swift
+++ Controller/Home/HomeViewController.swift
@@ -9,8 +9,33 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
+    // MARK: - Outlets
+    // Result
+    @IBOutlet weak var resultLabel: UILabel!
     
-    // MAR: - Initialization
+    // Numbers
+    @IBOutlet weak var number0: UIButton!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    @IBOutlet weak var numberDecimal: UIButton!
+    
+    // Operators
+    @IBOutlet weak var operatorAC: UIButton!
+    @IBOutlet weak var operatorPlusMinus: UIButton!
+    @IBOutlet weak var operatorPercent: UIButton!
+    @IBOutlet weak var operatorResult: UIButton!
+    @IBOutlet weak var operatorAddition: UIButton!
+    @IBOutlet weak var operatorSubstraction: UIButton!
+    @IBOutlet weak var operatorMultiplication: UIButton!
+    @IBOutlet weak var operatorDivision: UIButton!
+    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -19,10 +44,50 @@ final class HomeViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MAR: - Life Cycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    // MARK: Actions
+    // AC
+    @IBAction func operatorAcAction(_ sender: Any) {
         
+    }
+    // +/-
+    @IBAction func operatorPlusMinus(_ sender: Any) {
+        
+    }
+    // Porcentaje
+    @IBAction func operatorPercentAction(_ sender: Any) {
+        
+    }
+    // Resultado
+    @IBAction func operatorResultAction(_ sender: Any) {
+        
+    }
+    // Mas
+    @IBAction func operatorAdditionAction(_ sender: Any) {
+        
+    }
+    // Menos
+    @IBAction func operatorSubstractionAction(_ sender: Any) {
+        
+    }
+    // Multiplicación
+    @IBAction func operatorMultiplicationAction(_ sender: Any) {
+        
+    }
+    // División
+    @IBAction func operatorDivitionAction(_ sender: Any) {
+        
+    }
+    // Coma
+    @IBAction func operationSeparatorAction(_ sender: Any) {
+        
+    }
+    
+    // MARK: Number Action
+    @IBAction func numberAction(_ sender: UIButton) {
+        
+        print(sender.tag)
     }
 }
